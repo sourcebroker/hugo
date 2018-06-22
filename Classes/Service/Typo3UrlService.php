b606afa7f7e55e1bd4dac1666f09f356d33bf06f
Tomasz Stępniak <stempl88@gmail.com>
2018-06-22 16:35:26 +0200

    [FEATURE] Service for generating links based on typolink configuration

--- Classes/Service/Typo3UrlService.php
+++ Classes/Service/Typo3UrlService.php
@@ -24,14 +24,202 @@
 
 namespace SourceBroker\Hugo\Service;
 
+use Cocur\Slugify\Slugify;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\LinkHandling\LinkService;
+use TYPO3\CMS\Core\Resource\Exception\InvalidPathException;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\RootlineUtility;
+use TYPO3\CMS\Frontend\Page\PageRepository;
+use TYPO3\CMS\Frontend\Service\TypoLinkCodecService;
+
 class Typo3UrlService
 {
+
     /**
-     * @param $config
+     * @var \TYPO3\CMS\Core\Charset\CharsetConverter
+     */
+    protected $csConvertor;
+
+    /**
+     * @var PageRepository
+     *
+     */
+    protected $pageRepository;
+
+    /**
+     * @param \TYPO3\CMS\Core\Charset\CharsetConverter $csConvertor
+     */
+    public function injectCsConvertor(\TYPO3\CMS\Core\Charset\CharsetConverter $csConvertor)
+    {
+        $this->csConvertor = $csConvertor;
+    }
+
+    /**
+     * @param PageRepository $pageRepository
+     */
+    public function injectPageRepository(\TYPO3\CMS\Frontend\Page\PageRepository $pageRepository)
+    {
+        $this->pageRepository = $pageRepository;
+    }
+
+    /**
+     * @param     $config
+     * @param int $pageLanguageUid
+     *
+     * @return array|null
+     */
+    public function linkArray($config, int $pageLanguageUid): ?array
+    {
+        if (is_string($config)) {
+            $config = $this->buildTypolinkParams($config);
+        }
+
+        if (!is_array($config) || !$config) {
+            return null;
+        }
+
+        $additionalParams = $config['additionalParams'] ?? '';
+
+        parse_str(ltrim($additionalParams, '&'), $queryParts);
+
+        if (!$queryParts['L']) {
+            $queryParts['L'] = $pageLanguageUid;
+        }
+
+        $config['additionalParams'] = '&'.http_build_query($queryParts);
+
+        $linkService = GeneralUtility::makeInstance(LinkService::class);
+
+        try {
+            $linkDetails = $linkService->resolve($config['href']);
+        } catch (InvalidPathException $exception) {
+            return null;
+        }
+
+        list($href, $linkText) = $this->buildLink($linkDetails, $config);
+
+        $config['href'] = $href;
+        $config['tag'] = $this->buildTag($config, $linkText);
+
+        return $config;
+    }
+
+    protected function buildTypolinkParams(string $link): array
+    {
+        $config = [];
+        if ( ! empty($link)) {
+            $linkParameterParts = GeneralUtility::makeInstance(TypoLinkCodecService::class)->decode($link);
+
+            list($linkHandlerKeyword, $linkHandlerValue) = explode(':', $linkParameterParts['url'], 2);
+            $linkParameter = $linkParameterParts['url'];
+
+            $config = [
+                'href' => $linkParameter,
+                'target' => $linkParameterParts['target'],
+                'class' => $linkParameterParts['class'],
+                'title' => $linkParameterParts['title'],
+            ];
+
+            // additional parameters that need to be set
+            if ($linkParameterParts['additionalParams'] !== '') {
+                $forceParams = $linkParameterParts['additionalParams'];
+                // params value
+                $config['additionalParams'] .= $forceParams[0] === '&' ? $forceParams : '&' . $forceParams;
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * @param array $linkDetails
+     * @param array $config
+     *
+     * @return array
+     */
+    protected function buildLink(array $linkDetails, array $config): array
+    {
+        $linkType = $linkDetails['type'];
+
+        switch ($linkType) {
+            case 'page':
+                return $this->buildPageLink($linkDetails, $config);
+        }
+
+        return [];
+    }
+
+    /**
+     * @param array $linkDetails
+     * @param array $config
+     *
+     * @return array
+     */
+    protected function buildPageLink(array $linkDetails, array $config): array
+    {
+        $pageUid = $linkDetails['pageuid'];
+
+        $rootline = GeneralUtility::makeInstance(RootlineUtility::class, $pageUid)->get();
+        $page = current($rootline);
+        $additionalParams = $config['additionalParams'];
+        parse_str($additionalParams, $additionalParamsParts);
+        $languageUid = $additionalParamsParts['L'] ?? $page['sys_language_uid'];
+
+        $aliases = array_map(function (array $page) use ($languageUid) {
+            if (!$page['is_siteroot']) {
+                return $this->slug($page['title']);
+            }
+        }, array_reverse($this->pageRepository->getPagesOverlay($rootline, $languageUid)));
+
+        $url = '//'.$this->getDomainFor((int)$rootline[0]['uid']).'/'.implode('/', array_filter($aliases)).'/';
+
+        return [$url, $page['title']];
+    }
+
+    /**
+     * @param array       $config
+     * @param string|null $linkText
+     *
      * @return string
      */
-    public function link($config): string
+    protected function buildTag(array $config, string $linkText = null): string
     {
-        return '';
+        return '<a ' . GeneralUtility::implodeAttributes($config) . '>'.$linkText.'</a>';
+    }
+
+    /**
+     * @param int $pageUid
+     *
+     * @return string
+     */
+    protected function getDomainFor(int $pageUid) : string
+    {
+        $qb = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_domain');
+
+        $domain = $qb->select('domainName')
+            ->from('sys_domain')
+            ->andWhere(
+                $qb->expr()->eq('pid', $qb->createNamedParameter($pageUid))
+            )
+            ->orderBy('sorting', 'ASC')
+            ->execute()
+            ->fetch();
+
+        if (!$domain) {
+            throw new \RuntimeException('Missing domain for root page "'.$pageUid.'" ');
+        }
+
+        return $domain['domainName'];
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return string
+     */
+    protected function slug(string $title)
+    {
+        return Slugify::create()->slugify($title);
     }
 }
